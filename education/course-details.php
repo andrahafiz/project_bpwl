957c9483191f21643c29107dd22f4c1c0fe436d0
Andra Hafiz Hasibuan <andra18ti@mahasiswa.pcr.ac.id>
2019-07-24 22:28:28 +0700

    Update course-details.php

--- education/course-details.php
+++ education/course-details.php
@@ -1,3 +1,6 @@
+<?php
+$id = $_GET['id'];
+?>
 <!-- start banner Area -->
 <section class="banner-area relative about-banner" id="home">
     <div class="overlay overlay-bg"></div>
@@ -5,7 +8,7 @@
         <div class="row d-flex align-items-center justify-content-center">
             <div class="about-content col-lg-12">
                 <h1 class="text-white">
-                    Nama Guru
+                    Nama Guru <?php echo $id; ?>
                 </h1>
                 <p class="text-white link-nav"><a href="index.html">Home </a> <span class="lnr lnr-arrow-right"></span> <a href="courses.html"> Data Guru </a></p>
             </div>
