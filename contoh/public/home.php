744ffc84aa8f9db2bb0d6af83e704c059c806797
MArifCandra <arif18ti@mahasiswa.pcr.ac.id>
2019-07-24 21:50:56 +0700

    Home jumlah pegawai dan fasilitas

--- contoh/public/home.php
+++ contoh/public/home.php
@@ -15,11 +15,24 @@
                 <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
                     <div class="info-box bg-pink hover-expand-effect">
                         <div class="icon">
-                            <i class="material-icons">playlist_add_check</i>
+                            <i class="material-icons">account_circle</i>
                         </div>
                         <div class="content">
-                            <div class="text">NEW TASKS</div>
-                            <div class="number count-to" data-from="0" data-to="125" data-speed="15" data-fresh-interval="20">125</div>
+                            <div class="text">JUMLAH PEGAWAI</div>
+                             <?php
+                            include "pages/set/koneksi.php";
+                            $sql = "SELECT COUNT(guru_nama) FROM `guru`";
+                            $result =  mysqli_query($db, $sql);
+                            $row = mysqli_fetch_array($result);
+
+                            echo "
+                            
+                            <div class='number count-to' data-from='0' data-to='$row[0]' data-speed='15' 
+                            data-fresh-interval='20'>
+                            ";
+                           ?>
+
+                        </div>
                         </div>
                     </div>
                 </div>
@@ -29,8 +42,19 @@
                             <i class="material-icons">help</i>
                         </div>
                         <div class="content">
-                            <div class="text">NEW TICKETS</div>
-                            <div class="number count-to" data-from="0" data-to="257" data-speed="1000" data-fresh-interval="20">257</div>
+                            <div class="text">JUMLAH FASILITAS</div>
+                             <?php
+                            include "pages/set/koneksi.php";
+                            $sql = "SELECT COUNT(nama_fasilitas) FROM `fasilitas`";
+                            $result =  mysqli_query($db, $sql);
+                            $row = mysqli_fetch_array($result);
+
+                            echo "
+                            
+                            <div class='number count-to' data-from='0' data-to='$row[0]' data-speed='15' 
+                            data-fresh-interval='20'>
+                            ";
+                           ?>
                         </div>
                     </div>
                 </div>
