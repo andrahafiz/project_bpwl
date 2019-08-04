f22cf185c1d280a8b9deb3c193c52177ae76a73b
ronisamosir02 <52515573+ronisamosir02@users.noreply.github.com>
2019-08-04 17:54:23 +0700

    Update course-details.php

--- education/course-details.php
+++ education/course-details.php
@@ -1,19 +1,20 @@
-<?php
-include "pages/set/koneksi.php";
-$id = $_GET['id'];
-$sql = "select * from view_guru_detail where nip_guru=$id";
-$result = mysqli_query($db, $result);
-while ($row = mysqli_fetch_array($result)) {
-    ?>
+<!-- start banner Area -->
+<section class="banner-area relative about-banner" id="home">
+    <div class="overlay overlay-bg"></div>
+    <div class="container">
+        <div class="row d-flex align-items-center justify-content-center">
+            <div class="about-content col-lg-12">
+                <h1 class="text-white">
+                    <?php
+                    include "pages/set/koneksi.php";
+                    $id = $_GET['id'];
+                    $sql = "select *,b.photo_guru from view_guru_detail,guru b where nip_guru=$id";
+                    // echo $sql;
+                    $result = mysqli_query($db, $sql);
+                    while ($row = mysqli_fetch_array($result)) {
+                        ?>
+                        Nama Guru <?php echo $row[1]; ?>
 
-    <!-- start banner Area -->
-    <section class="banner-area relative about-banner" id="home">
-        <div class="overlay overlay-bg"></div>
-        <div class="container">
-            <div class="row d-flex align-items-center justify-content-center">
-                <div class="about-content col-lg-12">
-                    <h1 class="text-white">
-                        Nama Guru <?php echo $id; ?>
                     </h1>
                     <p class="text-white link-nav"><a href="index.html">Home </a> <span class="lnr lnr-arrow-right"></span> <a href="courses.html"> Popular Courses</a></p>
                 </div>
@@ -29,61 +30,62 @@ while ($row = mysqli_fetch_array($result)) {
             <div class="row">
                 <div class="col-lg-8 left-contents">
                     <div class="main-image">
-                        aaaa <img class="img-fluid" src="../contoh/public/pages/tables/images/Guru/<?php echo $row[2] ?>" alt="">
-                    </div>
-                    <div class="jq-tab-wrapper" id="horizontalTab">
+                        <img class="img-fluid" src="../contoh/public/pages/tables/images/Guru/<?php echo $row[5] ?>" alt="">
+                    </div><?php } ?>
+                <div class="jq-tab-wrapper" id="horizontalTab">
 
-                        <div class="jq-tab-content-wrapper" style=" margin-bottom: 50px;">
-                            <div class="jq-tab-content active" data-tab=" 1">
-                                When you enter into any new area of science, you almost always find yourself with a baffling new language of technical terms to learn before you can converse with the experts. This is certainly true in astronomy both in terms of terms that refer to the cosmos and terms that describe the tools of the trade, the most prevalent being the telescope.
-                                <br>
-                                <br>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.
-                            </div>
-                            <div class="jq-tab-content" data-tab="2">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.
-                                <br>
-                                <br>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.
-                            </div>
+                    <div class="jq-tab-content-wrapper" style=" margin-bottom: 50px;">
+                        <div class="jq-tab-content active" data-tab=" 1">
+                            When you enter into any new area of science, you almost always find yourself with a baffling new language of technical terms to learn before you can converse with the experts. This is certainly true in astronomy both in terms of terms that refer to the cosmos and terms that describe the tools of the trade, the most prevalent being the telescope.
+                            <br>
+                            <br>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.
+                        </div>
+                        <div class="jq-tab-content" data-tab="2">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.
+                            <br>
+                            <br>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 right-contents">
-                    <ul>
-                        <li>
-                            <a class="justify-content-between d-flex" href="#">
-                                <p>Nama Guru</p>
-                                <span class="or">George Mathews</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="justify-content-between d-flex" href="#">
-                                <p>Tanggal Lahir </p>
-                                <span>Belle Medhurst</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="justify-content-between d-flex" href="#">
-                                <p>Jabatan</p>
-                                <span>Haag and Sons</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="justify-content-between d-flex" href="#">
-                                <p>Alamat </p>
-                                <span>81401 Schumm Valley</span>
-                            </a>
-                        </li>
-                    </ul>
-                    <a href="#" class="primary-btn text-uppercase">Biodata Singkat</a>
-                </div>
+            </div>
+            <div class="col-lg-4 right-contents">
+                <ul>
+                    <li>
+                        <a class="justify-content-between d-flex" href="#">
+                            <p>Nama Guru</p>
+                            <span class="or">George Mathews</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="justify-content-between d-flex" href="#">
+                            <p>Tanggal Lahir </p>
+                            <span>Belle Medhurst</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="justify-content-between d-flex" href="#">
+                            <p>Jabatan</p>
+                            <span>Haag and Sons</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="justify-content-between d-flex" href="#">
+                            <p>Alamat </p>
+                            <span>81401 Schumm Valley</span>
+                        </a>
+                    </li>
+                </ul>
+                <a href="#" class="primary-btn text-uppercase">Biodata Singkat</a>
             </div>
         </div>
-    </section>
-    <!-- End course-details Area -->
-<?php
-} ?>
+    </div>
+
+
+</section>
+<!-- End course-details Area -->
+
 </body>
 
 </html>
