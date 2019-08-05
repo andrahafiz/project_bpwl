fd52d7ba25dd6ba84c4960f9e48f7cc3278ae91b
ronisamosir02 <52515573+ronisamosir02@users.noreply.github.com>
2019-08-05 07:05:50 +0700

    Update course-details.php

--- education/course-details.php
+++ education/course-details.php
@@ -8,7 +8,7 @@
                     <?php
                     include "pages/set/koneksi.php";
                     $id = $_GET['id'];
-                    $sql = "select *,b.photo_guru from view_guru_detail,guru b where nip_guru=$id";
+                    $sql = "select * from view_guru_detail where nip_guru=$id";
                     // echo $sql;
                     $result = mysqli_query($db, $sql);
                     while ($row = mysqli_fetch_array($result)) {
@@ -31,53 +31,65 @@
                 <div class="col-lg-8 left-contents">
                     <div class="main-image">
                         <img class="img-fluid" src="../contoh/public/pages/tables/images/Guru/<?php echo $row[5] ?>" alt="">
-                    </div><?php } ?>
-                <div class="jq-tab-wrapper" id="horizontalTab">
+                    </div>
+                    <div class="jq-tab-wrapper" id="horizontalTab">
 
-                    <div class="jq-tab-content-wrapper" style=" margin-bottom: 50px;">
-                        <div class="jq-tab-content active" data-tab=" 1">
-                            When you enter into any new area of science, you almost always find yourself with a baffling new language of technical terms to learn before you can converse with the experts. This is certainly true in astronomy both in terms of terms that refer to the cosmos and terms that describe the tools of the trade, the most prevalent being the telescope.
-                            <br>
-                            <br>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.
-                        </div>
-                        <div class="jq-tab-content" data-tab="2">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.
-                            <br>
-                            <br>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.
+                        <div class="jq-tab-content-wrapper" style=" margin-bottom: 50px;">
+                            <div class="jq-tab-content active" data-tab=" 1">
+
+                                Nama ( Singkatan ) : <?php echo $row[1] ?> ( <b><?php echo $row[2] ?></b> )
+                                <br />
+                                NIP : <?php echo $row[0] ?>
+                                <br />
+                                Jabatan : <?php echo $row[6] ?>
+                                <br />
+                                Alamat : <?php echo $row[9] ?>
+                                <br />
+                                Tempat Lahir : <?php echo $row[7] ?>
+                                <br />
+                                Tanggal Lahir : <?php echo $row[8] ?>
+                                <br />
+                                Pendidikan Terakhir : <?php echo $row[4] ?>
+
+
+
+
+                            </div>
+
                         </div>
                     </div>
+
                 </div>
-            </div>
-            <div class="col-lg-4 right-contents">
-                <ul>
-                    <li>
-                        <a class="justify-content-between d-flex" href="#">
-                            <p>Nama Guru</p>
-                            <span class="or">George Mathews</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a class="justify-content-between d-flex" href="#">
-                            <p>Tanggal Lahir </p>
-                            <span>Belle Medhurst</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a class="justify-content-between d-flex" href="#">
-                            <p>Jabatan</p>
-                            <span>Haag and Sons</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a class="justify-content-between d-flex" href="#">
-                            <p>Alamat </p>
-                            <span>81401 Schumm Valley</span>
-                        </a>
-                    </li>
-                </ul>
-                <a href="#" class="primary-btn text-uppercase">Biodata Singkat</a>
+                <div class="col-lg-4 right-contents">
+                    <ul>
+
+                        <li>
+                            <a class="justify-content-between d-flex" href="#">
+                                <p>NIP</p>
+                                <span><?php echo $row[0] ?></span>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="justify-content-between d-flex" href="#">
+                                <p>Nama Guru</p>
+                                <span class="or"><?php echo $row[1] ?></span>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="justify-content-between d-flex" href="#">
+                                <p>Tanggal Lahir </p>
+                                <span><?php echo $row[8] ?></span>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="justify-content-between d-flex" href="#">
+                                <p>Alamat </p>
+                                <span><?php echo $row[9] ?></span>
+                            </a>
+                        </li>
+                    </ul>
+                    <a href="#" class="primary-btn text-uppercase">Biodata Singkat</a>
+                <?php } ?>
             </div>
         </div>
     </div>
