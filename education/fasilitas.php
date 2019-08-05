09d68a71d7014283d5026af6436e4169093e60ea
MArifCandra <52515341+MArifCandra@users.noreply.github.com>
2019-08-05 07:59:17 +0700

    Update fasilitas.php

--- education/fasilitas.php
+++ education/fasilitas.php
@@ -1,16 +1,16 @@
 	<!-- start banner Area -->
 	<section class="banner-area relative about-banner" id="home">
-	    <div class="overlay overlay-bg"></div>
-	    <div class="container">
-	        <div class="row d-flex align-items-center justify-content-center">
-	            <div class="about-content col-lg-12">
-	                <h1 class="text-white">
-	                    Ektrakulikuler
-	                </h1>
-	                <p class="text-white link-nav"><a href="index.html">Home </a> <span class="lnr lnr-arrow-right"></span> <a href="gallery.html">Ektrakulikuler</a></p>
-	            </div>
-	        </div>
-	    </div>
+		<div class="overlay overlay-bg"></div>
+		<div class="container">
+			<div class="row d-flex align-items-center justify-content-center">
+				<div class="about-content col-lg-12">
+					<h1 class="text-white">
+						Fasilitas
+					</h1>
+					<p class="text-white link-nav"><a href="index.html">Home </a> <span class="lnr lnr-arrow-right"></span> <a href="gallery.html">Fasilitas</a></p>
+				</div>
+			</div>
+		</div>
 	</section>
 	<!-- End banner Area -->
 
@@ -21,44 +21,37 @@
 
 
 	<section class="top-category-widget-area pt-90 pb-90 ">
-	    <div class="container">
-	        <div class="row">
-	            <?php
+		<div class="container">
+			<div class="row">
+				<?php
 
-                include "pages/set/koneksi.php";
-                $sql = "SELECT * FROM `fasilitas`";
-                $query = mysqli_query($db, $sql);
-                $nourut = 1;
-                while ($row = mysqli_fetch_array($query)) {
-                    ?>
-	                <!-- <a href="..\contoh\public\pages\tables\images\Fasilitas\<?php echo $row[a2] ?>" class="img-gal">
-	                                            <div class="" relative">
-	                                                <div class="overlay overlay-bg"></div>
-	                                                <div class="relative">
-	                                                    <img class="img-fluid" src="..\contoh\public\pages\tables\images\Fasilitas\<?php echo $row[2] ?>" alt="">
-	                                                </div>
-	                                            </div>
-	                                        </a> -->
-	                <div class="col-lg-4">
-	                    <div class="single-cat-widget">
-	                        <div class="content relative">
-	                            <div class="overlay overlay-bg"></div>
-	                            <a href="..\contoh\public\pages\tables\images\Fasilitas\<?php echo $row[2] ?>">
-	                                <div class="thumb">
-	                                    <img class="content-image img-fluid d-block mx-auto" src="..\contoh\public\pages\tables\images\Fasilitas\<?php echo $row[2] ?>">
-	                                </div>
-	                                <div class="content-details">
-	                                    <h4 class="content-title mx-auto text-uppercase"><?php echo $row[1] ?></h4>
-	                                    <span></span>
-	                                    <p>Fasilitas</p>
-	                                </div>
-	                            </a>
-	                        </div>
-	                    </div>
-	                </div>
-	            <?php
-                }
-                ?>
-	        </div>
-	    </div>
+				include "pages/set/koneksi.php";
+				$sql = "SELECT * FROM `fasilitas`";
+				$query = mysqli_query($db, $sql);
+				$nourut = 1;
+				while ($row = mysqli_fetch_array($query)) {
+					?>
+
+					<div class="col-lg-4">
+						<div class="single-cat-widget">
+							<div class="content relative">
+								<div class="overlay overlay-bg"></div>
+								<a href="..\contoh\public\pages\tables\images\Fasilitas\<?php echo $row[2] ?>">
+									<div class="thumb">
+										<img class="content-image img-fluid d-block mx-auto" src="..\contoh\public\pages\tables\images\Fasilitas\<?php echo $row[2] ?>">
+									</div>
+									<div class="content-details">
+										<h4 class="content-title mx-auto text-uppercase"><?php echo $row[1] ?></h4>
+										<span></span>
+										<p>Fasilitas</p>
+									</div>
+								</a>
+							</div>
+						</div>
+					</div>
+				<?php
+				}
+				?>
+			</div>
+		</div>
 	</section>
